edd2a24f04afb37140e89089326d4ec8da36d98d
grindelwalt <sulxj600s@gmail.com>
2023-09-30 19:40:45 +0300

    Созадан класс генерации карты

---
+++ Source Code/Scripts/Game/GeneratinDestructBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD54.Scripts.Game
+{
+
+    public partial class GeneratinDestructBoard : CollisionObject2D
+    {
+        
+        //url Tile
+        public TileData[,] _tiles { get; private set; } = new TileData[5,5];
+
+    }
+}
