9b95f526023116028fff8ba2907fc92a9d5f9864
grindelwalt <sulxj600s@gmail.com>
2023-10-01 01:46:27 +0300

    not finish

--- Source Code/Scripts/Game/GeneratinDestructBoard.cs
+++ Source Code/Scripts/Game/GeneratinDestructBoard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aardvark.Base.Geometry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,27 @@ using System.Threading.Tasks;
 namespace LD54.Scripts.Game
 {
 
-    public partial class GeneratinDestructBoard : CollisionObject2D
+    public partial class GeneratinDestructBoard : Spatial 
     {
         
         //url Tile
         public TileData[,] _tiles { get; private set; } = new TileData[5,5];
+
+        public void GennerationBoardTiles()
+        {
+            for (int i = 0; i < 5; i++)
+                for (int d = 0; d < 5; d++)
+                {
+                    ColorRect tile = GD.Load<ColorRect>($"res://Assets/Templates/tile1.tscn");
+                    tile.Material = GD.Load<Material>($"res://Assets/Templates/tile1.tscn");
+                    tile.MarginTop
+                }
+            
+        }
+        private void RandomGenerator()
+        {
+
+        }
 
     }
 }
