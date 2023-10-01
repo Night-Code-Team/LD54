8b1a5fab52349e80f0390161104d66b81a3fd504
Alexander Kadykov <as.kadykov@mail.ru>
2023-10-01 13:57:49 +0300

    Стрельба

--- Source Code/Scripts/Game/Characters/MC.cs
+++ Source Code/Scripts/Game/Characters/MC.cs
@@ -1,15 +1,24 @@
 public partial class MC : Character
 {
-    public void GetInput()
+    private int Rotate()
     {
-        Vector2 inputDirection = Input.GetVector("a", "d", "w", "s");
-        Velocity = inputDirection * Vel;
+        if (Input.IsActionPressed("w"))
+            return 1;
+        if (Input.IsActionPressed("s"))
+            return 2;
+        if (Input.IsActionPressed("a"))
+            return 3;
+        if (Input.IsActionPressed("d"))
+            return 4;
+        return 0;
     }
     public override void _PhysicsProcess(double delta)
     {
-        GetInput();
-        MoveAndSlide();
+        Move();
+        Attack();
         GetNode<Camera2D>("/root/Game/Field/Camera").Position = Position;
+        Vector2 mousePos = GetViewport().GetMousePosition();
+        GetNode<ColorRect>("Aim").Position = new(mousePos.X - 980, mousePos.Y - 560);
     }
     public override void Spawn()
     {
@@ -21,11 +30,23 @@ public partial class MC : Character
     }
     public override void Move()
     {
-
+        Vector2 inputDirection = Input.GetVector("a", "d", "w", "s");
+        Velocity = inputDirection * Vel;
+        GetNode<AnimatedSprite2D>("Texture").Frame = Rotate();
+        MoveAndSlide();
     }
     public override void Attack()
     {
-
+        if (Input.IsActionJustPressed("ЛКМ"))
+        {
+            CharacterBody2D pizdyulina = GD.Load<PackedScene>("res://Assets/Templates/Objects/Pizdyulina.tscn").Instantiate<CharacterBody2D>();
+            pizdyulina.Position = Position;
+            Vector2 aimPos = GetViewport().GetMousePosition();
+            float angle = GetAngleTo(new(aimPos.X + Position.X - 960, aimPos.Y + Position.Y - 540));
+            pizdyulina.Rotation = angle;
+            pizdyulina.Velocity = new(1000 * (float)Math.Cos(angle), 1000 * (float)Math.Sin(angle));
+            GetNode("/root/Game/Field").AddChild(pizdyulina);
+        }
     }
     public override int HP { get; set; } = 3;
     public override int Vel { get; set; } = 400;
---
+++ Source Code/Scripts/Game/Objects/Pizdyulina.cs
@@ -0,0 +1,7 @@
+public partial class Pizdyulina : CharacterBody2D
+{
+	public override void _PhysicsProcess(double delta)
+	{
+		MoveAndSlide();
+	}
+}
