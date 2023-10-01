bf5472d7c53462be1ce4a1e989911c0bbdad326e
MartuS52 <maxikmax165@gmail.com>
2023-10-01 16:31:55 +0200

    Сделан генератор карты

--- Source Code/Scripts/Game/Terrain/Field.cs
+++ Source Code/Scripts/Game/Terrain/Field.cs
@@ -1,27 +1,66 @@
 public partial class Field : TileMap
 {
-	private readonly float startRadius = 2.5F;
 	[Export]
 	private Vector2I groundCords = new(0, 0);
 	[Export]
-	private Vector2I airCords = new(8, 0);
-
+	private Vector2I airCords = new(0, 1);
+	[Export]
+	private float PropabilityK = 0.32F;
+	[Export]
+	private float PropabilityB = 1.0F;
+	
 	public override void _Ready()
 	{
+		//Background
 		for (int x = -55; x < 55; x++)
 		{
 			for (int y = -55; y < 55; y++)
 			{
-				float distanceToCenter = Mathf.Sqrt(x * x + y * y);
-				if (distanceToCenter < startRadius)
+				SetCell(0, new Vector2I(x, y), 0, airCords);
+			}
+		}
+		AddLayer(1);
+		Generate();
+	}
+	private void OnTick()
+	{
+		for (int x = -4; x < 4; x++)
+		{
+			for (int y = -4; y < 4; y++)
+			{
+				if (GetCellSourceId(1, new Vector2I(x, y), false) != -1)
 				{
-					SetCell(0, new Vector2I(x, y), 0, groundCords);
+					
+				}
+			}
+		}
+		if (Input.IsActionPressed("regenerate"))
+			Generate();
+	}
+	public void Generate()
+	{
+		ClearLayer(1);
+		for (int x = -2; x <= 2; x++)
+		{
+			for (int y = -2; y <= 2; y++)
+			{
+				float distanceToCenter = Mathf.Sqrt(x * x + y * y);
+				if (distanceToCenter < 1)
+				{
+					SetCell(1, new Vector2I(x, y), 0, groundCords);
 				}
 				else
 				{
-					SetCell(0, new Vector2I(x, y), 0, airCords);
+					float DestroyPropability = PropabilityK*(distanceToCenter - PropabilityB);
+					if (GD.Randf() > DestroyPropability)
+					{
+						SetCell(1, new Vector2I(x, y), 0, groundCords);
+					}
 				}
 			}
 		}
 	}
 }
+
+
+
