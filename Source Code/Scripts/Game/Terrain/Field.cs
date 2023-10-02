34cc7953745ecf351a6669dc8b522b27f62a6591
Alexander Kadykov <as.kadykov@mail.ru>
2023-10-02 20:34:39 +0300

    СЛИВАТЬСЯ СЮДА

--- Source Code/Scripts/Game/Terrain/Field.cs
+++ Source Code/Scripts/Game/Terrain/Field.cs
@@ -1,187 +1,63 @@
-using Godot;
-using System;
-
 public partial class Field : TileMap
 {
-	[Export(PropertyHint.Range, "0,100,5")]
-	int iniChance;
-
-
-	[Export(PropertyHint.Range, "1,8,1")]
-	int birthLimit;
-
-	[Export(PropertyHint.Range, "1,8,1")]
-	int deathLimit;
-
-
-	[Export(PropertyHint.Range, "1,10,1")]
-	int numR;
-
-	[Export]
-	public Godot.TileMap topMap, bottomMap;
-	[Export]
-	public int TopTile, BotTile;
-	[Export]
-	public Vector2 tMapSize;
-
-
-	int width, height;
-	Random rnd;
-
-	Vector2[] BoundsInt;
-	int[,] terrainMap;
-
-
-
+	private Vector2I groundCords = new(0, 0);
+	private Vector2I airCords = new(0, 1);
+	private readonly float PropabilityK = 0.16F;
+	private readonly float PropabilityB = 1.0F;
+	private readonly int size = 5;
 
 	public override void _Ready()
 	{
-		rnd = new Random();
-		BoundsInt = new Vector2[9];
-		topMap = GetChild(0) as Godot.TileMap;
-		bottomMap = GetChild(1) as Godot.TileMap;
-
-
-
-		int j = 0;
-		for (int i = -1; i < 2; i++)
+		//Background
+		for (int x = -size * 10; x < size * 10; x++)
 		{
-			BoundsInt[j] = new Vector2(i, -1);
-			BoundsInt[j + 1] = new Vector2(i, 0);
-			BoundsInt[j + 2] = new Vector2(i, 1);
-
-			j += 3;
-
-		}
-	}
-
-	public void _Process(float delta)
-	{
-
-		if (Input.IsActionJustPressed("Mouse_left"))
-		{
-			doSim(numR);
-
-		}
-
-		if (Input.IsActionJustPressed("Mouse_right"))
-		{
-			PackedScene packed_scene = new PackedScene();
-			packed_scene.Pack(GetTree().CurrentScene);
-			ResourceSaver.Save(packed_scene, $"res://Assets/Scenes/Game/Game.tscn");
-
-		}
-		if (Input.IsActionJustPressed("Mouse_middle"))
-		{
-			//clearMap(true);
-
-		}
-	}
-
-	public void doSim(int numR)
-	{
-		clearMap(false);
-		width = (int)tMapSize.X;
-		height = (int)tMapSize.Y;
-		if (terrainMap == null)
-		{
-			terrainMap = new int[width, height];
-			initPos();
-		}
-
-		for (int i = 0; i < numR; i++)
-		{
-			terrainMap = neighbCheck(terrainMap);
-		}
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
+			for (int y = -size * 10; y < size * 10; y++)
 			{
-				//	if (terrainMap[x, y] == 1)
-				//	{
-				//		topMap.SetCell(-x + width / 2, -y + height / 2, TopTile);
-				//		topMap.SetCell(-x + width / 2, -y + height / 2, TopTile);
-				//	}
-				//	bottomMap.SetCell(-x + width / 2, -y + height / 2, BotTile);
-				//}
-				//topMap.UpdateBitmaskRegion(new Vector2(-width / 2, -height / 2), new Vector2(width / 2, height / 2));
+				SetCell(0, new Vector2I(x, y), 0, airCords);
 			}
 		}
-
-		int[,] neighbCheck(int[,] oldPos)
+		AddLayer(1);
+		Generate();
+	}
+	private void OnTick()
+	{
+		for (int x = -size; x < size; x++)
 		{
-			int[,] newPos = new int[width, height];
-			int neighb;
-
-			for (int x = 0; x < width; x++)
+			for (int y = -size; y < size; y++)
 			{
-
-				for (int y = 0; y < height; y++)
+				Vector2I CordsOnField = new Vector2I(x, y);
+				if (GetCellSourceId(1, CordsOnField, false) != -1)
 				{
-					neighb = 0;
-					foreach (Vector2 b in BoundsInt)
+					Vector2I CellType = GetCellAtlasCoords(1, CordsOnField, false);
+					if (CellType[1] == 0 && CellType[0] > 0 && CellType[0] < 8)
 					{
-						if (b.X == 0 && b.Y == 0) continue;
-						if (x + b.X >= 0 && x + b.X < width && y + b.Y >= 0 && y + b.Y < height)
+						SetCell(1, CordsOnField, 0, new Vector2I(CellType[0] + 1, CellType[1]));
+					}
+					else if (CellType[1] == 0 && CellType[0] == 8)
+					{
+						SetCell(1, CordsOnField, 0, new Vector2I(0, 1));
+					}
+					else if (CellType[1] == 0 && CellType[0] == 0)
+					{
+						if (GD.Randf() < 0.01F)
 						{
-							neighb += oldPos[x + (int)b.X, y + (int)b.Y];
-						}
-						else
-						{
-							neighb++;
-						}
-
-						if (oldPos[x, y] == 1)
-						{
-							if (neighb < deathLimit) newPos[x, y] = 0;
-							else
-							{
-								newPos[x, y] = 1;
-
-							}
-						}
-
-						if (oldPos[x, y] == 0)
-						{
-							if (neighb > birthLimit) newPos[x, y] = 1;
-							else
-							{
-								newPos[x, y] = 0;
-
-							}
+							SetCell(1, CordsOnField, 0, new Vector2I(1, 0));
 						}
 					}
 				}
 			}
-
-
-			return newPos;
-
 		}
-
-
-		void initPos()
+		if (Input.IsActionPressed("regenerate"))
+			Generate();
+	}
+	public void Generate()
+	{
+		ClearLayer(1);
+		for (int x = -size; x <= size; x++)
 		{
-			for (int x = 0; x < width; x++)
+			for (int y = -size; y <= size; y++)
 			{
-				for (int y = 0; y < height; y++)
-				{
-					terrainMap[x, y] = rnd.Next(1, 101) < iniChance ? 1 : 0;
-
-				}
-
-			}
-
-		}
-
-		void clearMap(bool complete)
-		{
-			topMap.Clear();
-			bottomMap.Clear();
-
-			if (complete)
-			{
-				terrainMap = null;
+				SetCell(1, new Vector2I(x, y), 0, groundCords);
 			}
 		}
 	}
