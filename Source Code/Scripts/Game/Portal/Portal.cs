187fe130f83430e77ba0f46d804c49320ce3b926
MartuS52 <maxikmax165@gmail.com>
2023-10-05 12:09:06 +0200

    изменен портал, добавлен спавн портала и ключей на карте

--- Source Code/Scripts/Game/Portal/Portal.cs
+++ Source Code/Scripts/Game/Portal/Portal.cs
@@ -1,11 +1,12 @@
 public partial class Portal : Node2D
 {
-	private int KeysLeft { get; set; } = 3;
+	private int KeysLeft = 3;
 	
 	public void OnKeyEntered(Node2D body)
 	{
 		KeysLeft -= 1;
 	}
+	
 	public void OnPlayerEntered(Node2D body)
 	{
 		if (KeysLeft <= 0)
@@ -13,6 +14,7 @@ public partial class Portal : Node2D
 			Win();
 		}
 	}
+	
 	public void Win()
 	{
 		GetTree().ChangeSceneToFile("res://Assets/Scenes/Game/Game.tscn");
--- Source Code/Scripts/Game/Terrain/Field.cs
+++ Source Code/Scripts/Game/Terrain/Field.cs
@@ -1,29 +1,54 @@
 public partial class Field : TileMap
 {
-	private Vector2I groundCords = new(0, 0);
-	private Vector2I airCords = new(0, 1);
-	private readonly float PropabilityK = 0.16F;
-	private readonly float PropabilityB = 1.0F;
-	private readonly int size = 10;
+	[Export]
+	private PackedScene PackedPortal;
+	
+	[Export]
+	private PackedScene PackedKey;
+	
+	private Node2D Portal;
+	
+	private List<Node2D> Keys = new List<Node2D>();
+	
+	private Vector2I GroundCords = new(0, 0);
+	
+	private Vector2I AirCords = new(0, 1);
+	
+	private readonly int Size = 4;
+	
+	private const float CellSize = 250;
+	
+	private const int KeyNumber = 3;
 
 	public override void _Ready()
 	{
-		//Background
-		for (int x = -size * 10; x < size * 10; x++)
+		for (int x = -Size * 10; x < Size * 10; x++)
 		{
-			for (int y = -size * 10; y < size * 10; y++)
+			for (int y = -Size * 10; y < Size * 10; y++)
 			{
-				SetCell(0, new Vector2I(x, y), 0, airCords);
+				SetCell(0, new Vector2I(x, y), 0, AirCords);
 			}
 		}
-		AddLayer(1);
-		Generate();
+		GenerateBase();
+		Portal = PackedPortal.Instantiate<Node2D>();
+		Portal.Position = Scale*CellSize/2;
+		AddChild(Portal);
+		float keysSpawnDirection = GD.Randf()*Mathf.Pi;
+		for (int i = 0; i < KeyNumber; i++)
+		{
+			Keys.Add(PackedKey.Instantiate<Node2D>());
+			float distanceFromCenter = (float)((Mathf.Sqrt(GD.Randf()/4)+0.4)*Size*CellSize);
+			Keys[i].Position = new Vector2(Mathf.Sin(keysSpawnDirection), Mathf.Cos(keysSpawnDirection)) * distanceFromCenter;
+			AddChild(Keys[i]);
+			keysSpawnDirection += Mathf.Pi*2/KeyNumber;
+		}
 	}
+	
 	private void OnTick()
 	{
-		for (int x = -size; x < size; x++)
+		for (int x = -Size; x < Size; x++)
 		{
-			for (int y = -size; y < size; y++)
+			for (int y = -Size; y < Size; y++)
 			{
 				Vector2I CordsOnField = new Vector2I(x, y);
 				if (GetCellSourceId(1, CordsOnField, false) != -1)
@@ -47,17 +72,17 @@ public partial class Field : TileMap
 				}
 			}
 		}
-		if (Input.IsActionPressed("regenerate"))
-			Generate();
 	}
-	public void Generate()
+	
+	public void GenerateBase()
 	{
+		AddLayer(1);
 		ClearLayer(1);
-		for (int x = -size; x <= size; x++)
+		for (int x = -Size; x <= Size; x++)
 		{
-			for (int y = -size; y <= size; y++)
+			for (int y = -Size; y <= Size; y++)
 			{
-				SetCell(1, new Vector2I(x, y), 0, groundCords);
+				SetCell(1, new Vector2I(x, y), 0, GroundCords);
 			}
 		}
 	}
